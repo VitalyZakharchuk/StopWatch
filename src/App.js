f236b7a47a3450a131e28cf4adb6a635f4289417
Vitalii-Zaharchuk <vitaliy.zakharchuk19@gmail.com>
2022-01-10 14:11:06 +0200

    rxjs

--- src/App.js
+++ src/App.js
@@ -1,62 +1,58 @@
-import React, {useState} from 'react';
-import DisplayComponent from './Components/display/DisplayComponent';
+import React from "react";
+import { useEffect, useState } from "react";
+import { interval, Subject } from "rxjs";
+import { takeUntil } from "rxjs/operators";
 import BtnComponent from './Components/btn/BtnComponent';
-import './App.css';
-
+import DisplayComponent from "./Components/display/DisplayComponent";
+ 
 function App() {
-  const [time, setTime] = useState({ s:0, m:0, h:0});
-  const [interv, setInterv] = useState();
+
+  const [time, setTime] = useState(0);
+  const [selected, setSelected] = useState("stop");
   
+  useEffect(() => {
+    const unsubscribe$ = new Subject();
+    interval(1000)
+      .pipe(takeUntil(unsubscribe$))
+      .subscribe(() => {
+        if (selected === "run") {
+          setTime(val => val + 1000);
+        }
+      });
+    return () => {
+      unsubscribe$.next();
+      unsubscribe$.complete();
+    };
+  }, [selected]);
+ 
   const start = () => {
-    run();
-    setInterv(setInterval(run, 1000));
+    setSelected("run");
   };
-
-  let updatedS = time.s, 
-      updatedM = time.m, 
-      updatedH = time.h;
-
-  const run = () => {
-    if(updatedM === 60){
-      updatedH++;
-      updatedM = 0;
-    }
-    if(updatedS === 60){
-      updatedM++;
-      updatedS = 0;
-    }
-    
-    updatedS++;
-    return setTime({ s:updatedS, m:updatedM, h:updatedH});
-  };
-
+ 
   const stop = () => {
-    clearInterval(interv);
-    setTime({ s:0, m:0, h:0})
-    
+    setSelected("stop");
+    setTime(0);
   };
-
-  const wait = () => {
-    clearInterval(interv);
-    
-    
+ 
+  const reset = () => {
+    setTime(0);
   };
-
-  const reset = () =>{
-    clearInterval(interv);
-    setTime({ s:0, m:0, h:0});
-  }
+ 
+  const wait =() => {
+    setSelected("wait");
+  };
  
   return (
     <div className="main-section">
-     <div className="clock-holder">
-          <div className="stopwatch">
-               <DisplayComponent time={time}/>
-               <BtnComponent   reset={reset} stop={stop} start={start} wait={wait}/>
-          </div>
-     </div>
+      <div className="clock-holder">
+        <div className="stopwatch">
+          <DisplayComponent time={time} />
+          <BtnComponent reset={reset} stop={stop} start={start} wait={wait} />
+        </div>
+      </div>
     </div>
   );
 }
 
 export default App;
+
--- src/Components/display/DisplayComponent.js
+++ src/Components/display/DisplayComponent.js
@@ -1,13 +1,11 @@
 import React from 'react';
+import './DisplayComponent.css';
 
 const DisplayComponent = ({time}) => {
   
   return (
     <div>
-       <span>{(time.h >= 10)? time.h : "0"+ time.h}</span>:
-       <span>{(time.m >= 10)? time.m : "0"+ time.m}</span>:
-       <span>{(time.s >= 10)? time.s : "0"+ time.s}</span>
-       
+      <span className='dispaly'> {new Date(time).toISOString().slice(11,19)}</span>
     </div>
   );
 }
